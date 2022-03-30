f979cd1ee7b19644204f2d115d81991a0b5b9cdb
qmercury <qmercury@knou.ac.kr>
2022-03-30 14:11:24 +0900

    유니티 0329 1번문제

---
+++ HD_qmercury_0329-1.cs/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace HD_qmercury_0329_1.cs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {// for를 이용하여 3단, 6단, 9단 구구단을 만드는 프로그램을 작성하기
+
+            for(int i = 3; i < 10; i = i + 3)
+            {
+                for(int j = 1; j <10; j++)
+                {
+                    Console.WriteLine($"{i} * {j} = {i * j}");
+
+                }
+            }
+            
+        }
+    }
+}
